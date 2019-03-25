427b4fcc8ca9d3273684fd37a076dd3039451345
Sweeper777 <sumulang@gmail.com>
2019-03-25 08:18:57 +0000

    added GateType.evaluate

--- Gately/GateType.swift
+++ Gately/GateType.swift
@@ -5,4 +5,12 @@ enum GateType {
     case or(_ operand: Signal)
     case not
     
+    func evaluate(operand: Signal) -> Signal {
+        switch self {
+        case .and(operand: let other): return operand && other
+        case .or(operand: let other): return operand || other
+        case .not: return !operand
+        }
+    }
+    
 }
