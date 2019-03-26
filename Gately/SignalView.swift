91fb2d351f53e2aaa1934b4d4db6f71b8e1d7d89
Sweeper777 <sumulang@gmail.com>
2019-03-26 07:07:37 +0000

    added SignalView.draw

--- Gately/SignalView.swift
+++ Gately/SignalView.swift
@@ -43,4 +43,30 @@ class SignalView: UIView {
             setNeedsDisplay()
         }
     }
+    
+    override func draw(_ rect: CGRect) {
+        let part1 = UIBezierPath()
+        part1.move(to: CGPoint(x: 0, y: startY))
+        part1.addLine(to: CGPoint(x: signalX, y: startY))
+        if signal {
+            UIColor.green.setStroke()
+        } else {
+            UIColor.black.setStroke()
+        }
+        part1.lineWidth = lineWidth
+        part1.stroke()
+        
+        let part2 = UIBezierPath()
+        part2.move(to: CGPoint(x: signalX, y: startY))
+        part2.addLine(to: CGPoint(x: breakpoint, y: startY))
+        part2.move(to: CGPoint(x: breakpoint, y: endY))
+        part2.addLine(to: CGPoint(x: bounds.width, y: endY))
+        UIColor.black.setStroke()
+        part2.lineWidth = lineWidth
+        part2.stroke()
+        
+        let circle = UIBezierPath(ovalIn: CGRect(x: signalX, y: startY, width: 0, height: 0).insetBy(dx: -10, dy: -10))
+        UIColor.red.setFill()
+        circle.fill()
+    }
 }
