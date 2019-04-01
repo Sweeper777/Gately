81e215be85ae0016ac659e03467ebea074969c1f
Sweeper777 <sumulang@gmail.com>
2019-04-01 08:11:23 +0100

    added some basic game objects

--- Gately/GameViewController.swift
+++ Gately/GameViewController.swift
@@ -2,55 +2,23 @@ import UIKit
 import SwiftyUtils
 
 class GameViewController: UIViewController {
-    @IBOutlet var signalView: SignalView!
-    
-    var displayLink: CADisplayLink!
-    var game: Game!
+    @IBOutlet var gameView: GameView!
     
     override func viewDidLoad() {
-        signalView.startY = signalView.bounds.height / 2
-        signalView.signalX = signalView.bounds.width * 0.1
-        signalView.endY = signalView.startY
-        
-        displayLink = CADisplayLink(target: self, selector: #selector(update))
-        
-        game = Game()
+        gameView.gameObjects.append(Line(position: CGPoint(x: 0, y: 0.5), velocity: (0, 0), zIndex: 1, length: 0.1, horizontal: true, color: UIColor.green))
+        gameView.gameObjects.append(Line(position: CGPoint(x: 0.1, y: 0.5), velocity: (-0.00001, 0), zIndex: 0, length: 0.4, horizontal: true, color: UIColor.black))
+        gameView.gameObjects.append(Line(position: CGPoint(x: 0.5, y: 0.55), velocity: (-0.00001, 0), zIndex: 0, length: 0.5, horizontal: true, color: UIColor.black))
+        gameView.gameObjects.append(Dot(position: CGPoint(x: 0.1, y: 0.5), zIndex: 2))
+        gameView.gameObjects.append(AndGate(position: CGPoint(x: 0.5, y: 0.55), velocity: (-0.00001, 0), zIndex: 3))
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        displayLink.add(to: .current, forMode: .common)
+        gameView.displayLink.add(to: .current, forMode: .common)
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        displayLink.remove(from: .current, forMode: .common)
-    }
-    
-    @objc func update() {
-        game.update()
-        if game.timeUntilReaching < 90 {
-            let gateView = signalView.subviews.first as? UIImageView ?? UIImageView(image: UIImage(named: game.currentlyShownGate.imageName()))
-            gateView.contentMode = .scaleAspectFit
-            let size = CGSize(width: signalView.height / 2, height: signalView.height / 2)
-            let x = signalView.signalX + (signalView.width - signalView.signalX) * (game.timeUntilReaching / 30.0).f
-            let endY = signalView.startY + size.height * newYFactor(for: game.currentlyShownGate).f
-            let y = endY - size.height / 2
-            gateView.frame = CGRect(origin: CGPoint(x: x, y: y), size: size)
-            if !signalView.subviews.contains(gateView) {
-                signalView.addSubview(gateView)
-            }
-            signalView.endY = endY
-            signalView.breakpoint = x + (x + size.width) / 2
-        }
-    }
-    
-    func newYFactor(for gate: GateType) -> Double {
-        switch gate {
-        case .and, .or:
-            return 0.2
-        case .not:
-            return 0
-        }
+        gameView.displayLink.remove(from: .current, forMode: .common)
     }
 }
