f98e3e9fec0451a37f6ea2901b2ede7e5f038dbf
Sweeper777 <sumulang@gmail.com>
2019-04-03 11:21:36 +0100

    added a shouldBeDeleted method in GameObject

--- Gately/GameObject.swift
+++ Gately/GameObject.swift
@@ -7,5 +7,12 @@ protocol GameObject : class {
     
     func draw(in rect: CGRect)
     
-    func isOutOf(rect: CGRect) -> Bool
+    func frame(whenDrawnIn rect: CGRect) -> CGRect
+}
+
+extension GameObject {
+    func shouldBeDeleted(from rect: CGRect) -> Bool {
+        let myFrame = frame(whenDrawnIn: rect)
+        return myFrame.x + myFrame.width < 0 || myFrame.y > rect.height
+    }
 }
