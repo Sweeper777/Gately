fc5b3001a58931781fa3f133db27a9804244e18a
Sweeper777 <sumulang@gmail.com>
2019-04-04 09:42:23 +0100

    edited viewDidLoad

--- Gately/GameViewController.swift
+++ Gately/GameViewController.swift
@@ -18,11 +18,15 @@ class GameViewController: UIViewController {
     }
     
     override func viewDidLoad() {
-        gameView.gameObjects.append(Line(position: CGPoint(x: 0, y: 0.5), velocity: (0, 0), zIndex: 1, length: 0.1, horizontal: true, color: UIColor.green))
-        gameView.gameObjects.append(Line(position: CGPoint(x: 0.1, y: 0.5), velocity: (-0.00001, 0), zIndex: 0, length: 0.4, horizontal: true, color: UIColor.black))
-        gameView.gameObjects.append(Line(position: CGPoint(x: 0.5, y: 0.55), velocity: (-0.00001, 0), zIndex: 0, length: 0.5, horizontal: true, color: UIColor.black))
-        gameView.gameObjects.append(Dot(position: CGPoint(x: 0.1, y: 0.5), zIndex: 2))
-        gameView.gameObjects.append(AndGate(position: CGPoint(x: 0.5, y: 0.55), velocity: (-0.00001, 0), zIndex: 3))
+        gameView.delegate = self
+        signalLine = Line(position: CGPoint(x: 0, y: 0.5), velocity: (0, 0), zIndex: 1, length: 0.1, horizontal: true, color: .green)
+        gameView.gameObjects.append(signalLine)
+        dot = Dot(position: CGPoint(x: 0.1, y: 0.5), zIndex: 2)
+        gameView.gameObjects.append(dot)
+        lastLineObject = Line(position: CGPoint(x: 0, y: 0.5), velocity: (speed, 0), zIndex: 0, length: 1, horizontal: true, color: .black)
+        gameView.gameObjects.append(lastLineObject)
+        addNewGameObjects()
+        addNewGameObjects()
     }
     
     func addNewGameObjects() {
