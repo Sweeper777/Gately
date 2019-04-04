514fe33b7b4844ec4768c4c1506fa1526d4200c3
Sweeper777 <sumulang@gmail.com>
2019-04-04 09:42:38 +0100

    conformed GameVC to GameViewDelegate

--- Gately/GameViewController.swift
+++ Gately/GameViewController.swift
@@ -59,3 +59,21 @@ class GameViewController: UIViewController {
         gameView.displayLink.remove(from: .current, forMode: .common)
     }
 }
+
+extension GameViewController : GameViewDelegate {
+    func gateDidLeaveScreen(gameView: GameView, gate: Gate) {
+        addNewGameObjects()
+    }
+    
+    func gameViewDidUpdate(gameView: GameView) {
+        let dotAndSignalLineFrame = dot.frame(whenDrawnIn: gameView.bounds).union(signalLine.frame(whenDrawnIn: gameView.bounds))
+        let gates = gameView.gameObjects.compactMap { $0 as? Gate }
+        if let gate = gates.first (where: {
+            let frame = $0.frame(whenDrawnIn: gameView.bounds)
+            return frame.contains(dotAndSignalLineFrame) || frame.x < dotAndSignalLineFrame.x
+        }) {
+            signalLine.position.y = gate.position.y
+            dot.position.y = gate.position.y
+        }
+    }
+}
