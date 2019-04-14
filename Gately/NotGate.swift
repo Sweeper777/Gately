121906b7b9ac889a6e27b821743f24f10b5a4348
Sweeper777 <sumulang@gmail.com>
2019-04-14 10:13:30 +0100

    implemented NotGate.draw(in:)

--- Gately/NotGate.swift
+++ Gately/NotGate.swift
@@ -17,4 +17,34 @@ class NotGate: Gate {
     
     let size: CGSize = CGSize(width: 0.25 * 8.0 / 7.0, height: 0.25)
     
+    func draw(in rect: CGRect) {
+        let path = UIBezierPath()
+        let actualHeight = rect.height * size.height
+        let actualWidth = rect.height * size.width
+        let actualCenterX = rect.width * position.x
+        let actualCenterY = rect.height * position.y
+        let actualTopLeftX = actualCenterX - actualWidth / 2
+        let actualTopLeftY = actualCenterY - actualHeight / 2
+        
+        let pointyBitCoordinates = CGPoint(x: actualTopLeftX + actualHeight, y: actualCenterY)
+        path.move(to: pointyBitCoordinates)
+        path.addLine(to: CGPoint(x: actualTopLeftX, y: actualTopLeftY + actualHeight))
+        path.addLine(to: CGPoint(x: actualTopLeftX, y: actualTopLeftY))
+        path.addLine(to: pointyBitCoordinates)
+        let radius = (actualWidth - actualHeight) / 2
+        path.addArc(
+            withCenter: CGPoint(x: pointyBitCoordinates.x + radius, y: actualCenterY),
+            radius: radius,
+            startAngle: .pi,
+            endAngle: -.pi,
+            clockwise: false)
+        path.close()
+        
+        UIColor.black.setStroke()
+        UIColor.white.setFill()
+        path.lineWidth = 5
+        path.close()
+        path.fill()
+        path.stroke()
+    }
 }
