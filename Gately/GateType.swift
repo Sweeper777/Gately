27d81e4be2ae5cc8eb0637f159c33020e0c75fa2
Sweeper777 <sumulang@gmail.com>
2019-04-16 08:25:54 +0100

    cleaned up some of the code in GateType

--- Gately/GateType.swift
+++ Gately/GateType.swift
@@ -8,29 +8,10 @@ enum GateType {
     
     func evaluate(operand: Signal) -> Signal {
         switch self {
-        case .and(operand: let other): return operand && other
-        case .or(operand: let other): return operand || other
+        case .and(let other): return operand && other
+        case .or(let other): return operand || other
+        case .xor(let other): return operand != other
         case .not: return !operand
         }
-    }
-    
-    func imageName() -> String {
-        switch self {
-        case .and: return "AND"
-        case .or: return "OR"
-        case .not: return "NOT"
-        }
-    }
-    
-    static let allTypes: [GateType] = [
-        .and(true),
-        .and(false),
-        .or(true),
-        .or(false),
-        .not,
-    ]
-    
-    static func random() -> GateType {
-        return allTypes.randomElement()!
     }
 }
