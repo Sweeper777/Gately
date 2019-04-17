bc5297efbebf971f0dc96a1a19b09648351cfc98
Sweeper777 <sumulang@gmail.com>
2019-04-17 09:04:50 +0100

    added didSendSignal delegate method

--- Gately/GameViewDelegate.swift
+++ Gately/GameViewDelegate.swift
@@ -1,4 +1,5 @@
 protocol GameViewDelegate: class {
     func gateDidLeaveScreen(gameView: GameView, gate: Gate)
     func gameViewDidUpdate(gameView: GameView)
+    func didSendSignal(gameView: GameView, signal: Signal)
 }
