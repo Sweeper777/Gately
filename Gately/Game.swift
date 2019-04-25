b24f88ccdb2906ccf18ed406b46fff5dbce80406
Sweeper777 <sumulang@gmail.com>
2019-04-25 07:27:47 +0100

    added Game.init

--- Gately/Game.swift
+++ Gately/Game.swift
@@ -3,6 +3,14 @@ import UIKit
 class Game {
     weak var delegate: GameDelegate?
     
+    var gameObjects: [GameObject] = [] {
+        didSet {
+            let sorted = gameObjects.map { $0.zIndex }.sorted()
+            if gameObjects.map({ $0.zIndex }) != sorted {
+                gameObjects.sort { $0.zIndex < $1.zIndex }
+            }
+        }
+    }
     var speed: CGFloat = -0.33333333 {
         didSet {
            delegate?.speedDidChange(self, newSpeed: speed)
@@ -29,6 +37,22 @@ class Game {
         didSet {
             delegate?.scoreDidChange(self, newScore: score)
         }
+    }
+    
+    var whRatio: CGFloat
+    
+    init(whRatio: CGFloat) {
+        self.whRatio = whRatio
+        signalLine = Line(position: CGPoint(x: 0, y: 0.5), velocity: (0, 0), zIndex: 1, length: 0.1, horizontal: true, color: .green)
+        dot = Dot(position: CGPoint(x: 0.1, y: 0.5), zIndex: 2)
+        lastLineObject = Line(position: CGPoint(x: 0, y: 0.5), velocity: (speed, 0), zIndex: 0, length: 1, horizontal: true, color: .gray)
+        
+        gameObjects.append(signalLine)
+        gameObjects.append(dot)
+        gameObjects.append(lastLineObject)
+        
+        addNewGameObjects()
+        addNewGameObjects()
     }
     
 }
