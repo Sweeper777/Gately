f77bb1187d25413eef8e794a049518e57667702f
Sweeper777 <sumulang@gmail.com>
2019-04-26 06:55:42 +0100

    added new members to GameViewDelegate

--- Gately/GameViewDelegate.swift
+++ Gately/GameViewDelegate.swift
@@ -2,4 +2,8 @@ protocol GameViewDelegate: class {
     func gateDidLeaveScreen(gameView: GameView, gate: Gate)
     func gameViewDidUpdate(gameView: GameView)
     func didSendSignal(gameView: GameView, signal: Signal)
+    
+    func removeAllGameObjects(where predicate: (GameObject) -> Bool)
+    
+    var gameObjects: [GameObject] { get }
 }
